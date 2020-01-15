cc6aadce66607797cde5a9631ec1f6fb1b10cab6
KenEdoLB <kenedolaksamana@gmail.com>
2020-01-15 14:23:30 +0700

    coba commit

--- application/controllers/Barang.php
+++ application/controllers/Barang.php
@@ -20,6 +20,19 @@ class Barang extends CI_Controller
     $this->load->view('index', $data);
   }
 
+  public function store(){
+    $data = [
+      'nama_barang' => $this->input->post('nama_barang'),
+      'id_barang' => $this->input->post('id_barang'),
+      'user' => '089612994819',
+      'status' => 0
+    ];
+
+    $this->db->insert('kategori', $data);
+    $this->session->set_flashdata('success', 'Kategori berhasil ditambahkan');
+    redirect(base_url('kategori'));
+  }
+
 }
 
 
--- application/views/pages/barang/index.php
+++ application/views/pages/barang/index.php
@@ -3,10 +3,32 @@
         <div class="row">
             <div class="col-12">
                 <div class="card">
-                    <div class="card-body">
-                        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ex, blanditiis tenetur ratione quia porro deserunt eius provident corporis consequatur sint assumenda laudantium corrupti mollitia molestias? Voluptatem inventore assumenda rem quibusdam?
+                <div class="card-header">
+                        <h3 class="card-title">Buat Barang Baru</h3>
+                        <div class="card-tools">
+                            <button type="button" class="btn btn-tool" data-card-widget="collapse" data-toggle="tooltip" title="Collapse">
+                                <i class="fa fa-minus"></i>
+                            </button>
+                        </div>
                     </div>
                 </div>
+
+                <div class="card-body">
+                        <form method="post" action="<?= base_url('barang/store') ?>">
+                            <div class="row">
+                                <div class="col">
+                                    <label for="nama_kategori">Nama Kategori</label>
+                                    <input type="text" class="form-control" id="nama_kategori" name="nama_kategori" placeholder="Nama Kategori">
+                                </div>
+                                <div class="col">
+                                    <label for="jenis_kategori">Jenis Kategori</label>
+                                    <input type="text" class="form-control" id="jenis_kategori" name="jenis_kategori" placeholder="Jenis Kategori (Optional)">
+                                </div>
+                            </div>
+                            <br>
+                            <button type="submit" class="btn btn-primary pull-right">Submit</button>
+                        </form>
+                    </div>
             </div>
         </div>
         <div class="row">
@@ -30,7 +52,17 @@
                                 <?php $no=1; foreach ($barangs->result() as $barang) : ?>
                                     <tr>
                                         <td><?= $no++?></td>
-                                        <td><?= $barang->kode_barang ?></td>
+                                        <td><?= $barang->kodebarang ?></td>
+                                        <td><?= $barang->id_kategori ?></td>
+                                        <td><?= $barang->nama_barang ?></td>
+                                        <td><?= $barang->hargabeli ?></td>
+                                        <td><?= $barang->hargajual ?></td>
+                                        <td><?= $barang->stok ?></td>
+                                        <td align="center" style="min-width: 150px">
+                                            <a href="" class="btn btn-primary"><i class="fa fa-eye"></i></a>
+                                            <a href="" class="btn btn-warning"><i class="fa fa-edit"></i></a>
+                                            <a href="" class="btn btn-danger"><i class="fa fa-trash"></i></a>
+                                        </td>
                                     </tr>
                                 <?php endforeach; ?>
                             </tbody>
